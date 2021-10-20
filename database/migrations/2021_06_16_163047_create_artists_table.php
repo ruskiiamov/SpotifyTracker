63edda2febc7c1b4ca03853f5ff2f1c10a5c6660
ruslan-kiiamov <xkijam@gmail.com>
2021-10-20 23:07:22 +0300

    nullable for checked_at added

--- database/migrations/2021_06_16_163047_create_artists_table.php
+++ database/migrations/2021_06_16_163047_create_artists_table.php
@@ -17,7 +17,7 @@ class CreateArtistsTable extends Migration
             $table->id();
             $table->string('spotify_id')->unique();
             $table->string('name');
-            $table->dateTime('checked_at');
+            $table->dateTime('checked_at')->nullable();
             $table->timestamps();
         });
     }
