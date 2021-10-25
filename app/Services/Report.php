fe3d1b9a2c66c886e58c118b65f0ce6b27b80185
ruslan-kiiamov <xkijam@gmail.com>
2021-10-25 23:57:03 +0300

    report class added with tetst

--- app/Services/Report.php
+++ app/Services/Report.php
@@ -2,7 +2,62 @@
 
 namespace App\Services;
 
+use Exception;
+
 class Report
 {
+    /**
+     * Report fields container
+     *
+     * @var array
+     */
+    private $report = ['error_messages' => []];
 
+    /**
+     * Report fields initialization
+     *
+     * @param ...$fields
+     */
+    public function __construct(...$fields)
+    {
+        foreach ($fields as $field) {
+            $this->report[$field] = 0;
+        }
+    }
+
+    /**
+     * Report counter increment
+     *
+     * @param $name
+     * @param $arguments
+     * @throws Exception
+     */
+    public function __call($name, $arguments): void
+    {
+        if (isset($this->report[$name])) {
+            $this->report[$name]++;
+        } else {
+            throw new Exception("{$name} - not valid report field");
+        }
+    }
+
+    /**
+     * Set new error message
+     *
+     * @param string $message
+     */
+    public function setErrorMessage(string $message): void
+    {
+        $this->report['error_messages'][] = $message;
+    }
+
+    /**
+     * Get final report
+     *
+     * @return array
+     */
+    public function getReport(): array
+    {
+        return $this->report;
+    }
 }
--- tests/Unit/ReportTest.php
+++ tests/Unit/ReportTest.php
@@ -2,17 +2,44 @@
 
 namespace Tests\Unit;
 
+use App\Services\Report;
 use PHPUnit\Framework\TestCase;
 
 class ReportTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    private function initializeReport(): Report
     {
-        $this->assertTrue(true);
+        return new Report('first_field', 'second_field');
+    }
+
+    public function testInitialization()
+    {
+        $report = $this->initializeReport();
+        $this->assertEquals(0, $report->getReport()['first_field']);
+        $this->assertEquals(0, $report->getReport()['second_field']);
+    }
+
+    public function testCounter()
+    {
+        $report = $this->initializeReport();
+        $report->first_field();
+        $report->first_field();
+        $report->second_field();
+        $this->assertEquals(2, $report->getReport()['first_field']);
+        $this->assertEquals(1, $report->getReport()['second_field']);
+    }
+
+    public function testErrorMessage()
+    {
+        $report = $this->initializeReport();
+        $message = 'Test Error Message';
+        $report->setErrorMessage($message);
+        $this->assertEquals($message, $report->getReport()['error_messages'][0]);
+    }
+
+    public function testReport()
+    {
+        $report = $this->initializeReport();
+        $this->assertIsArray($report->getReport());
     }
 }
