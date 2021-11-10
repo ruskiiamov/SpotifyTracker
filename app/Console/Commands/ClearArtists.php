0936d7fd89788a9177285ec9d74c18b127e817d4
ruslan-kiiamov <xkijam@gmail.com>
2021-11-11 00:31:50 +0300

    feat: Add report for ClearArtist command

--- app/Console/Commands/ClearArtists.php
+++ app/Console/Commands/ClearArtists.php
@@ -3,10 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Services\Tasks;
+use App\Traits\ConsoleReport;
 use Illuminate\Console\Command;
 
 class ClearArtists extends Command
 {
+    use ConsoleReport;
+
     /**
      * The name and signature of the console command.
      *
@@ -39,9 +42,11 @@ class ClearArtists extends Command
     {
         $this->line('Clearing...');
         $startTime = time();
-        (new Tasks())->clearArtists();
+        $report = (new Tasks())->clearArtists();
         $endTime = time();
         $duration = $endTime - $startTime;
-        $this->info('Success: Artists table cleared | time: ' . $duration . ' seconds');
+        $this->info('Success: Artists table cleared');
+        $this->info('Time: ' . $duration . ' seconds');
+        $this->showReport($report->getReport());
     }
 }
--- app/Services/Tasks.php
+++ app/Services/Tasks.php
@@ -197,139 +197,39 @@ class Tasks
     }
 
     /**
-     * Get artist last album (3 attempts)
-     *
-     * @param string $accessToken
-     * @param string $spotifyId
-     * @return stdClass
+     * @return Report
      */
-    private function getLastAlbum(string $accessToken,string $spotifyId): stdClass
+    public function clearArtists(): Report
     {
-        $result = Spotify::getLastArtistAlbum($accessToken, $spotifyId);
-        $counter = 0;
-        while ($result === null && $counter < 2) {
-            sleep(0.3);
-            $result = Spotify::getLastArtistAlbum($accessToken, $spotifyId);
-            $counter++;
-        }
-        return $result->items[0];
-    }
-
-
-    private function isReleaseDateOk($lastAlbum)
-    {
-        $releaseDateThreshold = $this->getReleaseDateThreshold();
-        return ($lastAlbum->release_date_precision === 'day' && $lastAlbum->release_date > $releaseDateThreshold);
-    }
-
-    private function updateConnections(Artist $artist, $genres)
-    {
-        $this->addGenres($genres);
-        foreach ($genres as $genre) {
-            Connection::firstOrCreate([
-                'artist_id' => $artist->id,
-                'genre_id' => Genre::where('name', $genre)->first()->id,
-            ]);
-        }
-
-        $connections = $artist->connections;
-        foreach ($connections as $connection) {
-            if (!in_array($connection->genre->name, $genres)) {
-                $connection->delete();
-            }
-        }
-    }
-
-    private function addGenres($genres) //TODO shift that code to updateConnections method
-    {
-        foreach ($genres as $genre) {
-            Genre::firstOrCreate(
-                ['name' => $genre],
-                ['category_id' => Category::where('name', $this->setGenreCategory($genre))->first()->id],
-            );
-        }
-    }
-
-    private function getReleaseDateThreshold()
-    {
-        return date('Y-m-d', time() - $this->releaseAge * 24 * 60 * 60);
-    }
-
-    private function getCheckDateTimeThreshold()
-    {
-        return date('Y-m-d H:i:s', time() - $this->checkAge * 60 * 60);
-    }
-
-    public function genresAnalyse()
-    {
-        $words = [];
-        $genres = Genre::all();
-        foreach ($genres as $genre) {
-            $separated = explode(' ', strtolower($genre->name));
-            foreach ($separated as $item) {
-                if (array_key_exists($item, $words)) {
-                    $words[$item]++;
-                } else {
-                    $words[$item] = 1;
-                }
-            }
-        }
-        arsort($words);
-        $result[] = [];
-        foreach ($words as $word => $amount) {
-            $result[] = [$word, $amount];
-        }
-        return $result;
-    }
-
-    private function setGenreCategory($genre)
-    {
-        foreach ($this->genreCategories as $genreCategory => $keyWords) {
-            foreach ($keyWords as $keyWord) {
-                if (str_contains(strtolower($genre), $keyWord)) {
-                    return $genreCategory;
-                }
-            }
-        }
-        return 'other';
-    }
-
-    public function clearArtists()
-    {
-        Artist::chunk(200, function ($artists) {
+        $report = new Report('deleted_artists');
+        Artist::chunk(200, function ($artists) use (&$report) {
             foreach ($artists as $artist) {
-                $following = $artist->followings->first();
-                $album = $artist->albums->first();
-                if (is_null($following) && is_null($album)) {
-                    $connections = $artist->connections;
-                    foreach ($connections as $connection) {
-                        $connection->delete();
+                try {
+                    $following = $artist->followings->first();
+                    $album = $artist->albums->first();
+                    if (is_null($following) && is_null($album)) {
+                        $connections = $artist->connections;
+                        foreach ($connections as $connection) {
+                            $connection->delete();
+                        }
+                        $artist->delete();
+                        $report->deleted_artists();
                     }
-                    $artist->delete();
+                } catch (\Exception $e) {
+                    $report->setErrorMessage($artist->name . ': ' . $e->getMessage());
+                    continue;
                 }
             }
         });
+        return $report;
     }
 
     /**
-     *
-     * Check album name for banned words: live, deluxe, etc
-     *
-     * @param string $albumName
-     * @return bool
+     * @return Report
      */
-    private function isAlbumNameOk(string $albumName): bool
+    public function addNewReleases(): Report //TODO - this method should be reworked and optimized !!!
     {
-        foreach ($this->exceptions as $exception) {
-            if (str_contains(strtolower($albumName), $exception)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public function addNewReleases()
-    {
+        $report = new Report();
         $refreshToken = User::first()->refresh_token;
         $accessToken = Spotify::getRefreshedAccessToken($refreshToken);
 
@@ -381,9 +281,147 @@ class Tasks
                 }
             }
         } while ($offset <= 950);
+        return $report;
     }
 
-    private function isGenreSubscribed($genreNames)
+    /**
+     * @return array
+     */
+    public function genresAnalyse(): array
+    {
+        $words = [];
+        $genres = Genre::all();
+        foreach ($genres as $genre) {
+            $separated = explode(' ', strtolower($genre->name));
+            foreach ($separated as $item) {
+                if (array_key_exists($item, $words)) {
+                    $words[$item]++;
+                } else {
+                    $words[$item] = 1;
+                }
+            }
+        }
+        arsort($words);
+        $result[] = [];
+        foreach ($words as $word => $amount) {
+            $result[] = [$word, $amount];
+        }
+        return $result;
+    }
+
+    /**
+     * @param string $accessToken
+     * @param string $spotifyId
+     * @return stdClass
+     */
+    private function getLastAlbum(string $accessToken,string $spotifyId): stdClass
+    {
+        $result = Spotify::getLastArtistAlbum($accessToken, $spotifyId);
+        $counter = 0;
+        while ($result === null && $counter < 2) {
+            sleep(0.3);
+            $result = Spotify::getLastArtistAlbum($accessToken, $spotifyId);
+            $counter++;
+        }
+        return $result->items[0];
+    }
+
+    /**
+     * @param $lastAlbum
+     * @return bool
+     */
+    private function isReleaseDateOk($lastAlbum): bool
+    {
+        $releaseDateThreshold = $this->getReleaseDateThreshold();
+        return ($lastAlbum->release_date_precision === 'day' && $lastAlbum->release_date > $releaseDateThreshold);
+    }
+
+    /**
+     * @param Artist $artist
+     * @param $genres
+     */
+    private function updateConnections(Artist $artist, $genres): void
+    {
+        $this->addGenres($genres);
+        foreach ($genres as $genre) {
+            Connection::firstOrCreate([
+                'artist_id' => $artist->id,
+                'genre_id' => Genre::where('name', $genre)->first()->id,
+            ]);
+        }
+
+        $connections = $artist->connections;
+        foreach ($connections as $connection) {
+            if (!in_array($connection->genre->name, $genres)) {
+                $connection->delete();
+            }
+        }
+    }
+
+    /**
+     * @param $genres
+     */
+    private function addGenres($genres): void //TODO shift that code to updateConnections method
+    {
+        foreach ($genres as $genre) {
+            Genre::firstOrCreate(
+                ['name' => $genre],
+                ['category_id' => Category::where('name', $this->setGenreCategory($genre))->first()->id],
+            );
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getReleaseDateThreshold(): string
+    {
+        return date('Y-m-d', time() - $this->releaseAge * 24 * 60 * 60);
+    }
+
+    /**
+     * @return string
+     */
+    private function getCheckDateTimeThreshold(): string
+    {
+        return date('Y-m-d H:i:s', time() - $this->checkAge * 60 * 60);
+    }
+
+    /**
+     * @param $genre
+     * @return string
+     */
+    private function setGenreCategory($genre): string
+    {
+        foreach ($this->genreCategories as $genreCategory => $keyWords) {
+            foreach ($keyWords as $keyWord) {
+                if (str_contains(strtolower($genre), $keyWord)) {
+                    return $genreCategory;
+                }
+            }
+        }
+        return 'other';
+    }
+
+    /**
+     * @param string $albumName
+     * @return bool
+     */
+    private function isAlbumNameOk(string $albumName): bool
+    {
+        foreach ($this->exceptions as $exception) {
+            if (str_contains(strtolower($albumName), $exception)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param $genreNames
+     * @return bool
+     */
+    private function isGenreSubscribed($genreNames): bool
     {
         foreach ($genreNames as $genreName) {
             $genre = Genre::where('name', $genreName)->first();
@@ -397,8 +435,6 @@ class Tasks
     }
 
     /**
-     * Get access token for specified user
-     *
      * @param User $user
      * @return string
      */
@@ -410,8 +446,6 @@ class Tasks
     }
 
     /**
-     * Get access token for random user
-     *
      * @return string
      */
     private function getAccessToken(): string
