1551e895aaaf50b236d2417a2fdf5e7ba2956a76
ruslan-kiiamov <xkijam@gmail.com>
2022-02-22 08:28:33 +0300

    feat: Create ClearArtists Job

---
+++ app/Jobs/ClearArtists.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Services\Tracker;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ClearArtists implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public int $tries = 3;
+
+    /**
+     * Calculate the number of seconds to wait before retrying the job.
+     *
+     * @return array
+     */
+    public function backoff(): array
+    {
+        return [1, 3, 5];
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param Tracker $tracker
+     * @return void
+     */
+    public function handle(Tracker $tracker)
+    {
+        $tracker->clearArtists();
+    }
+}
