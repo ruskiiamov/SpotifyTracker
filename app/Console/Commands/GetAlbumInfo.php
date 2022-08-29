83856b60ee2b8b01883c53e1b3338658920b08ce
ruslan-kiiamov <xkijam@gmail.com>
2022-08-29 10:23:25 +0300

    [chore] Add get album info console command

---
+++ app/Console/Commands/GetAlbumInfo.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Facades\Spotify;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class GetAlbumInfo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:get-album-info {album_id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get album info from Spotify';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        $albumId = $this->argument('album_id');
+        $field = 'available_markets';
+
+        if (!isset($albumId)) {
+            $this->error('Album id required');
+            return;
+        }
+
+        if (Cache::has('client_access_token')) {
+            $accessToken = Cache::get('client_access_token');
+        } else {
+            $accessToken = Spotify::getClientAccessToken();
+        }
+
+        $fullAlbum = Spotify::getAlbum($accessToken, $albumId);
+
+        print_r($fullAlbum->$field);
+    }
+}
